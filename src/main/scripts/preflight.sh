1fc98e538dd72636717c877f0f1d36a06dc65a0b
Jianguo Ma <jiangma@microsoft.com>
2022-12-07 14:19:55 +0800

    fail fast with appropriate error message if aadObjectId is not retrieved
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- src/main/scripts/preflight.sh
+++ src/main/scripts/preflight.sh
@@ -16,6 +16,12 @@
 
 set -Eeuo pipefail
 
+# Fail fast the deployment if object Id of the service principal is empty
+if [[ -z "$AAD_OBJECT_ID" ]]; then
+  echo "The object Id of the service principal you just created is not successfully retrieved, please retry another deployment using its client id ${AAD_CLIENT_ID}." >&2
+  exit 1
+fi
+
 # Wait 30s for service principal available after creation
 # See https://github.com/WASdev/azure.liberty.aro/issues/59 & https://github.com/WASdev/azure.liberty.aro/issues/79
 sleep 30
