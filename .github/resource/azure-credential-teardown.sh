15f3a7a4abb35ed25aaa9df47d17fcef03b1c180
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-28 09:46:01 +0800

    Updated azure-credential-teardown.sh script to delete additional Azure credentials and service principal name.

--- .github/resource/azure-credential-teardown.sh
+++ .github/resource/azure-credential-teardown.sh
@@ -9,5 +9,5 @@ echo "Execute azure-credential-teardown.sh - Start------------------------------
 gh secret delete "AZURE_CREDENTIALS"
 AZURE_CREDENTIALS_SP_NAME=$(gh variable get "AZURE_CREDENTIALS_SP_NAME")
 az ad sp delete --id $(az ad sp list --display-name $AZURE_CREDENTIALS_SP_NAME --query "[].appId" -o tsv| tr -d '\r\n')
-
+gh variable delete "AZURE_CREDENTIALS_SP_NAME"
 echo "Execute azure-credential-teardown.sh - End--------------------------------------------"
