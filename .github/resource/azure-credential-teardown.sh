4cfb671c1964ac1544266c47d7cb0d526d749f39
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-28 10:48:42 +0800

    Refactor azure-credential-teardown.sh to use az ad app delete instead of az ad sp delete and improve variable handling

--- .github/resource/azure-credential-teardown.sh
+++ .github/resource/azure-credential-teardown.sh
@@ -8,6 +8,6 @@ echo "Execute azure-credential-teardown.sh - Start------------------------------
 
 gh secret delete "AZURE_CREDENTIALS"
 AZURE_CREDENTIALS_SP_NAME=$(gh variable get "AZURE_CREDENTIALS_SP_NAME")
-az ad sp delete --id $(az ad sp list --display-name $AZURE_CREDENTIALS_SP_NAME --query "[].appId" -o tsv| tr -d '\r\n')
+az ad app delete --id $(az ad sp list --display-name "$AZURE_CREDENTIALS_SP_NAME" --query '[].appId' -o tsv| tr -d '\r\n')
 gh variable delete "AZURE_CREDENTIALS_SP_NAME"
 echo "Execute azure-credential-teardown.sh - End--------------------------------------------"
